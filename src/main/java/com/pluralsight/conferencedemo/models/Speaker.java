4f48e7b9552add6268937983207395bb371b3112
swarnendudas <swarnendu.das@gmail.com>
2021-07-29 01:36:41 -0400

    Complete API

--- src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
+++ src/main/java/com/pluralsight/conferencedemo/models/Speaker.java
@@ -1,9 +1,14 @@
 package com.pluralsight.conferencedemo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Type;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity(name = "speakers")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Speaker {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,7 +19,24 @@ public class Speaker {
     private String company;
     private String speaker_bio;
 
+    // Lob annotation helps JPA to deal with large data. Lob stands for Large Objects
+    // 2nd annotation helps hibernate to deal with binary data. Without this annotation
+    // exception would occur when JPA queries the data and tries to push it into the
+    // session instance.
+    @Lob
+    @Type(type = "org.hibernate.type.BinaryType")
+    private byte[] speaker_photo;
+
+    public byte[] getSpeaker_photo() {
+        return speaker_photo;
+    }
+
+    public void setSpeaker_photo(byte[] speaker_photo) {
+        this.speaker_photo = speaker_photo;
+    }
+
     @ManyToMany(mappedBy = "speakers")
+    @JsonIgnore
     private List<Session> sessions;
 
     public List<Session> getSessions() {
